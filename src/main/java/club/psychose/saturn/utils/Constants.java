1b0ff31c65c64f3dcaf253f24b9a09cbe1ee488b
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2021-12-18 21:15:31 +0100

    Updated version.

--- src/main/java/club/psychose/saturn/utils/Constants.java
+++ src/main/java/club/psychose/saturn/utils/Constants.java
@@ -22,7 +22,7 @@ import java.nio.file.Paths;
 import java.util.Locale;
 
 public class Constants {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String BUILD = "1";
 
     public static Path getSaturnFolderPath (String additionalPath) {
